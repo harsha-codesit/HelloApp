2292226fe7de866b747ea88bb81d37d70878e333
harsha-codesit <harshadha2018@gmail.com>
2026-03-26 09:27:59 +0530

    uc3 implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
 
-public class  Main{
+public class HelloApp {
     public static void main(String[] args) {
 
-        String name = args[0];
+        // Default value
+        String name = "World";
 
+        // If argument is provided, use it
+        if (args.length > 0) {
+            name = args[0];
+        }
+
+        // Print result
         System.out.println("Hello, " + name + "!");
     }
 }
