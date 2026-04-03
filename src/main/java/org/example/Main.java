0c3c2c87df63695a33355becc5c6e9f257ef3f19
harsha-codesit <harshadha2018@gmail.com>
2026-04-03 11:56:50 +0530

    UC7 added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,13 @@
 package org.example;
 
 public class HelloApp {
+
     public static void main(String[] args) {
-
-        String output;
-
-        // If no arguments → default
         if (args.length == 0) {
-            output = "Hello, World!";
+            System.out.println("Hello, World!");
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-
-            // Append all names with ", "
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            // Remove trailing ", "
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-            output = "Hello, " + names + "!";
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
-
-        // Print result
-        System.out.println(output);
     }
 }
